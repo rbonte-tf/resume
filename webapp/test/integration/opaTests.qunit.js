e04c7d355f70f4a6ea89374962d82c2032577435
rbonte-tf <bonte_ruben@yahoo.com>
2023-08-12 12:26:01 +0300

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["cv/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"cv/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
