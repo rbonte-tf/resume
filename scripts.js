ff775d1fd77664d331d014b80e7c06e95b32c283
rbonte-tf <bonte_ruben@yahoo.com>
2023-08-13 10:40:30 +0300

    animations

--- scripts.js
+++ scripts.js
@@ -32,18 +32,40 @@ document.addEventListener("DOMContentLoaded", function() {
         projectCards.forEach(card => {
             let cardSkills = card.querySelectorAll(".skill");
             let hasSkill = Array.from(cardSkills).some(s => s.textContent === skill);
+            
             if (hasSkill) {
+                card.style.opacity = "1";          // New line: For Fade-In effect
+                card.style.transform = "scale(1)";  // New line: Reset to original size
                 card.style.display = "block";
             } else {
-                card.style.display = "none";
+                card.style.opacity = "0";                      // New line: For Fade-Out effect
+                card.style.transform = "scale(0.95)";          // New line: Slightly reduces size
+                setTimeout(() => {                             // New block: Delay hiding the card
+                    if (card.style.opacity === "0") {
+                        card.style.display = "none"; 
+                    }
+                }, 500); // This delay should match your transition duration
             }
         });
         filterMessage.innerHTML = `Currently showing <strong>${skill}</strong> projects | <span id="resetFilter" style="cursor: pointer; font-weight: bold; color: #007bff;">Click here to reset.</span>`;
         filterMessage.style.display = "block";
     
-        document.getElementById("resetFilter").addEventListener("click", function() {
-            projectCards.forEach(card => card.style.display = "block");
+        // Update resetFilter since the innerHTML changed.
+        resetFilter = document.getElementById("resetFilter");  // Add this line.
+        resetFilter.addEventListener("click", function() {
+            projectCards.forEach(card => {
+                card.style.opacity = "0";              // New line: Start Fade-Out effect before revealing all cards
+            });
+        
+            setTimeout(() => {                         // New block: Delay revealing the card
+                projectCards.forEach(card => {
+                    card.style.opacity = "1";          // Fade-In effect
+                    card.style.transform = "scale(1)";  // Reset to original size
+                    card.style.display = "block";
+                });
+            }, 500); // This delay should match your transition duration
+        
             filterMessage.style.display = "none";
-        });
+        });        
     }    
 });
